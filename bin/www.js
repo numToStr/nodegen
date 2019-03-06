151ce5f57e889c315db7cb247f8b233548103005
vkasraj <vikasraj1911@gmail.com>
2019-03-06 13:36:19 +0530

    bin: first program

--- bin/www.js
+++ bin/www.js
@@ -1,3 +1,26 @@
 #!/usr/bin/env node
+"use strict";
 
-console.log("Hello World");
+const fs = require("fs");
+const path = require("path");
+const program = require("commander");
+const { version } = require("../package.json");
+
+program
+    .version(version, "-v, --version")
+    .command("init <dir>")
+    .description("Initialize project directory")
+    .option("-r, --recursive", "Remove recursively")
+    .action((dir, cmd) => {
+        const dirPath = path.join(".", dir);
+
+        fs.mkdir(dirPath, error => {
+            if (error) {
+                throw error;
+            }
+
+            console.log("Directory created: %s", dir);
+        });
+    });
+
+program.parse(process.argv);
