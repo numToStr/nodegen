d7c0467c275790388c1f4b7bb64d7754852707c8
vkasraj <vikasraj1911@gmail.com>
2019-03-07 12:17:11 +0530

    app: creating bin

--- bin/www.js
+++ bin/www.js
@@ -6,21 +6,57 @@ const path = require("path");
 const program = require("commander");
 const { version } = require("../package.json");
 
+const TEMPLATE_DIR = path.join(__dirname, "..", "template");
+
 program
     .version(version, "-v, --version")
     .command("init <dir>")
     .description("Initialize project directory")
     .option("-r, --recursive", "Remove recursively")
     .action((dir, cmd) => {
-        const dirPath = path.join(".", dir);
-
-        fs.mkdir(dirPath, error => {
-            if (error) {
-                throw error;
-            }
-
-            console.log("Directory created: %s", dir);
-        });
+        mkdir(".", dir);
     });
 
 program.parse(process.argv);
+
+/**
+ * Make the given dir relative to base.
+ *
+ * @param {string} base
+ * @param {string} dir
+ */
+
+function mkdir(base, dir) {
+    const dirPath = path.join(base, dir);
+    fs.mkdirSync(dirPath);
+
+    copyTemplate("bin/www.js", `${dirPath}/www.js`);
+
+    console.log("\n Project created: %s", dir);
+}
+
+/**
+ * Copy file from template directory.
+ */
+function copyTemplate(from, to) {
+    // Make dest directory
+    // Read files from source dir
+    // write files to dest dir
+    const filesPath = path.join(TEMPLATE_DIR, from);
+
+    const files = fs.readFileSync(filesPath);
+
+    write(to, files, "utf-8");
+}
+
+/**
+ * For creating file onto the disk
+ *
+ * @param {String} file
+ * @param {String} str
+ */
+
+function write(file, str) {
+    fs.writeFileSync(file, str);
+    console.log("> \x1b[36mcreated\x1b[0m : " + file);
+}
---
+++ template/bin/www.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const http = require("http");
+
+// Port Environment variable
+const PORT = process.env.PORT || 8080;
+
+// Creating the node server
+const SERVER = http.createServer();
+
+// Firing up the server on selected port
+SERVER.listen(PORT);
+
+SERVER.on("listening", () => {
+    console.log("[Server]::LISTEN:%s", PORT);
+});
+
+// Callback function for checking connecting or error
+SERVER.on("error", error => {
+    throw new Error(`[Server]::ERROR:${error.message}`);
+});
+
+/**
+ * For Handling unhandled promise rejection
+ *
+ * If any rejection occurs in the app,
+ * then the server will forcefully shutdown
+ * Ex: Like if the app is unable to connect to database then the app will shutdown.
+ */
+process.on("unhandledRejection", reason => {
+    // I just caught an unhandled promise rejection,
+    // since we already have fallback handler for unhandled errors (see below),
+    // let throw and let him handle that
+    console.log("[Unhandled Rejection]::", reason.message);
+
+    throw reason;
+});
+
+process.on("uncaughtException", error => {
+    // I just received an error that was never handled,
+    // time to handle it and then decide whether a restart is needed
+    console.log("[Uncaught Exception]::", error.message);
+
+    throw error;
+});
