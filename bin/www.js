5ee49f20a41f6af589099b08349ff3e7aa90d119
vkasraj <vikasraj1911@gmail.com>
2019-03-08 12:33:31 +0530

    cli: creating nested files

--- bin/www.js
+++ bin/www.js
@@ -15,6 +15,12 @@ program
     .option("-r, --recursive", "Remove recursively")
     .action((dir, cmd) => {
         mkdir(".", dir);
+        mkdir(dir, "bin");
+
+        copyTemplateMulti({
+            from: "bin",
+            to: `${dir}/bin`
+        });
     });
 
 program.parse(process.argv);
@@ -29,34 +35,59 @@ program.parse(process.argv);
 function mkdir(base, dir) {
     const dirPath = path.join(base, dir);
     fs.mkdirSync(dirPath);
+}
 
-    copyTemplate("bin/www.js", `${dirPath}/www.js`);
+/**
+ * Copy multiple files from template directory.
+ */
 
-    console.log("\n Project created: %s", dir);
+function copyTemplateMulti({ from, to, nameGlob }) {
+    const binDir = path.join(TEMPLATE_DIR, from);
+
+    fs.readdir(binDir, { encoding: "utf8" }, (err, files) => {
+        if (err) {
+            throw err;
+        }
+
+        files.forEach(file => {
+            copyTemplate({
+                file,
+                from,
+                to
+            });
+        });
+    });
+
+    // fs.readdirSync(path.join(TEMPLATE_DIR, fromDir))
+    //     .filter(minimatch.filter(nameGlob, { matchBase: true }))
+    //     .forEach(function(name) {
+    //         copyTemplate(path.join(fromDir, name), path.join(toDir, name));
+    //     });
 }
 
 /**
  * Copy file from template directory.
  */
-function copyTemplate(from, to) {
+function copyTemplate({ file, from, to }) {
     // Make dest directory
     // Read files from source dir
     // write files to dest dir
-    const filesPath = path.join(TEMPLATE_DIR, from);
+    const filesPath = path.join(TEMPLATE_DIR, from, file);
+    const content = fs.readFileSync(filesPath);
 
-    const files = fs.readFileSync(filesPath);
+    const destPath = path.join(to, file);
 
-    write(to, files, "utf-8");
+    write(content, destPath, "utf-8");
 }
 
 /**
  * For creating file onto the disk
  *
- * @param {String} file
- * @param {String} str
+ * @param {String} fileName Name of the destination file
+ * @param {String} context Content of the file to be written in new file
  */
 
-function write(file, str) {
-    fs.writeFileSync(file, str);
-    console.log("> \x1b[36mcreated\x1b[0m : " + file);
+function write(content, fileName) {
+    fs.writeFileSync(fileName, content);
+    console.log("> \x1b[36mcreated\x1b[0m : " + fileName);
 }
