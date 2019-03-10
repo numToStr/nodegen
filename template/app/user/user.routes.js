30bfe29d9083d9a0c67ba36385f03d14d1b28685
vkasraj <vikasraj1911@gmail.com>
2019-03-10 20:08:38 +0530

    template: made ejs component in app

--- template/app/user/user.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-const { createUser, getUser } = require("./user.controllers");
-
-router.post("/", createUser);
-router.get("/:id", getUser);
-
-module.exports = router;
